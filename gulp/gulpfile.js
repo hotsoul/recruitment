cbbf00dcc17ae1974df81def5be612cc77ab1b77
Hiroshi Wakasugi <hiro4.to.pyonkiti@gmail.com>
2018-05-28 22:59:19 +0900

    html作成開始

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,1 +1,35 @@
-const gulp = require('gulp');
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var htmlmin = require('gulp-htmlmin');
+//var htmltidy = require('gulp-htmltidy');
+
+// clean
+gulp.task('clean', function () {
+    return gulp.src(['../docs/*', '!../docs/robots.txt'], {read: false})
+        .pipe(clean({force: true}));
+});
+
+// htmltidy
+// おそらくコンテナにtidyが入っていないせいで、errorになってしまう
+/*
+gulp.task('htmltidy', function () {
+    return gulp.src('../src/*.html')
+        .pipe(htmltidy({doctype: 'html5',
+                        hideComments: true,
+                        indent: false}))
+        .pipe(gulp.dest('../docs/'));
+});
+*/
+
+// htmlmin
+gulp.task('htmlmin', function () {
+    return gulp.src('../src/*.html')
+        .pipe(htmlmin({collapseWhitespace: true, 
+                        removeComments: true}))
+        .pipe(gulp.dest('../docs/'));
+});
+
+// watch
+gulp.task('watch', function () {
+    gulp.watch('../src/*.html', ['htmlmin']);
+});

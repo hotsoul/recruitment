3597918d7628fc651132aecafc14dbd74bb4f196
Hiroshi Wakasugi <hiro4.to.pyonkiti@gmail.com>
2018-06-15 10:45:29 +0900

    sassに着手

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
+var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 var htmlmin = require('gulp-htmlmin');
 //var htmltidy = require('gulp-htmltidy');
 
@@ -7,6 +13,18 @@ var htmlmin = require('gulp-htmlmin');
 gulp.task('clean', function () {
     return gulp.src(['../docs/*', '!../docs/robots.txt'], {read: false})
         .pipe(clean({force: true}));
+});
+
+// sass
+gulp.task('sass', function () {
+    return gulp.src('../src/assets/sass/**/*.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sassGlob())
+        .pipe(sass({outputstyle: 'compressed'}))
+        .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('../docs/assets/css'));
 });
 
 // htmltidy
@@ -31,5 +49,6 @@ gulp.task('htmlmin', function () {
 
 // watch
 gulp.task('watch', function () {
+    gulp.watch('../src/assets/sass/**/*.scss', ['sass']);
     gulp.watch('../src/*.html', ['htmlmin']);
 });
